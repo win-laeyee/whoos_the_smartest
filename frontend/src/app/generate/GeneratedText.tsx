03e0b790624b2f3118efac934de0d5c079dc8d3e
sharmaine1028 <sharmaine1028@gmail.com>
2024-07-22 23:53:04 +0800

    Add generate page

--- frontend/src/app/generate/GeneratedText.tsx
+++ frontend/src/app/generate/GeneratedText.tsx
@@ -1,19 +1,31 @@
 import Markdown from "react-markdown";
+import remarkGfm from "remark-gfm";
 
 interface GeneratedTextProps {
   markdown?: string;
 }
 
 const GeneratedText: React.FC<GeneratedTextProps> = ({ markdown: string }) => {
-  const exampleMarkdown = `A paragraph with *emphasis* and **strong importance**.`;
+  const exampleMarkdown = `A paragraph with *emphasis* and **strong importance**.
+
+  > A block quote with ~strikethrough~ and a URL: https://reactjs.org.
+
+* Lists
+* [ ] todo
+* [x] done
+
+A table:
+
+| a | b |
+| - | - |`;
   return (
-    <div className="flex flex-row justify-start items-start m-5 h-screen">
-      <div className="card bg-base-100 h-4/5 basis-1/2 overflow-y-auto p-5">
-        <Markdown>{exampleMarkdown}</Markdown>
-      </div>
-      <div className="flex flex-col justify-center items-center h-4/5">
-        <h1>Notes has been generated...</h1>
-      </div>
+    <div className="flex flex-row justify-start items-start mx-5 w-screen h-screen prose">
+      <Markdown
+        className="bg-base-100 h-4/5 w-1/2 overflow-y-auto p-5 mr-5 flex-grow"
+        remarkPlugins={[remarkGfm]}
+      >
+        {exampleMarkdown}
+      </Markdown>
     </div>
   );
 };
--- frontend/src/app/generate/page.tsx
+++ frontend/src/app/generate/page.tsx
@@ -1,11 +1,30 @@
+"use client";
+
 import Layout from "../ui/layout";
 import GeneratedText from "./GeneratedText";
+import { useRouter } from "next/navigation";
 
 const Page: React.FC = () => {
+  const router = useRouter();
+
+  const handleNavigate = () => {
+    router.push("/quiz");
+  };
+
   return (
-    <div className="flex h-full flex-col items-center bg-primary min-h-screen">
+    <div className="flex h-full flex-col justify-center items-center bg-primary w-screen min-h-screen">
       <Layout />
-      <GeneratedText />
+      <div className="flex w-full h-full">
+        <GeneratedText />
+        <div className="flex flex-col w-1/2 h-full prose mr-5 justify-center">
+          <h1>Notes has been generated...</h1>
+          <button className="btn btn-secondary mb-5">Download</button>
+          <h1>Once you have reviewed, go ahead to quiz yourself!</h1>
+          <button className="btn btn-secondary" onClick={handleNavigate}>
+            Quiz
+          </button>
+        </div>
+      </div>
     </div>
   );
 };

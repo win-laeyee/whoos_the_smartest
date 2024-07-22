f746c2ee415dcaf22646b2940d53e7c8ee703004
sharmaine1028 <sharmaine1028@gmail.com>
2024-07-22 23:52:36 +0800

    Add placeholder page for quiz page

---
+++ frontend/src/app/quiz/page.tsx
@@ -0,0 +1,12 @@
+import Layout from "../ui/layout";
+
+const Page: React.FC = () => {
+  return (
+    <div className="bg-primary min-h-screen min-w-screen">
+      <Layout></Layout>
+      <div>Quiz</div>
+    </div>
+  );
+};
+
+export default Page;

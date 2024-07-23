3ff334333db86712d7104a36378bb75e4c0774c0
win-laeyee <winlae.yee@u.nus.edu>
2024-07-23 23:23:51 +0800

    Initialise llm and configure logging for fastapi

---
+++ backend/src/utils/app_init.py
@@ -0,0 +1,34 @@
+import json
+import logging
+import google.generativeai as genai
+
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+
+def init_gemini_llm():
+    with open('backend/secrets/google_gemini_credentials.json') as f:
+        secrets = json.load(f)
+
+    GOOGLE_API_KEY=secrets['GOOGLE_API_KEY']
+    genai.configure(api_key=GOOGLE_API_KEY)
+
+    model = genai.GenerativeModel('gemini-1.5-pro')
+    return model
+
+
+
+
+def configure_logging(log_level: int = logging.INFO):
+    logging.getLogger().handlers.clear()
+
+    logging.basicConfig(
+        format="%(asctime)s [%(process)d] [%(levelname)s] %(message)s",
+        datefmt="[%Y-%m-%d %H:%M:%S]",
+        level=log_level,
+    )
+
+    logging.getLogger("uvicorn.access").handlers.clear()
+    logging.getLogger("uvicorn.error").handlers.clear()
+    logging.getLogger("uvicorn.access").propagate = True
+    logging.getLogger("uvicorn.error").propagate = True
+

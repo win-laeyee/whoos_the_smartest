491fcb51b63e99851e9e90ad2c8c67840ec0e9a0
win-laeyee <winlae.yee@u.nus.edu>
2024-07-25 00:13:50 +0800

    Set up FastAPI endpoint for generating notes and to delete videos

--- backend/src/api/v1/app.py
+++ backend/src/api/v1/app.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
-from backend.src.utils.app_init import init_gemini_llm
+
+from backend.src.utils.app_init import configure_genai
+from backend.src.utils.generation import generate_notes
 from backend.src.api.v1.models.requests import SampleRequest
 from backend.src.api.v1.models.responses import SampleResponse
+
+import google.generativeai as genai
 
 app = FastAPI()
 
@@ -10,14 +14,20 @@ def healthcheck():
     return {"status": "ok"}
 
 
-@app.post("/api/test", response_model=SampleResponse)
-def test(
+@app.post("/api/get-notes-from-uploaded-file", response_model=SampleResponse)
+def get_notes_from_uploaded_file(
     request_body: SampleRequest
 ):
-    model = init_gemini_llm()
-    response = model.generate_content('Tell me a story about a magic backpack')
+    response = generate_notes(request_body.random_string)
 
-    return SampleResponse(random_string=response.text)
+    return SampleResponse(random_string=response)
 
+@app.post("/api/delete-video")
+def delete_video(
+    request_body: SampleRequest
+):
+    configure_genai()
+    genai.delete_file(request_body.random_string)
 
+    return SampleResponse(random_string="Video file deleted.")
 

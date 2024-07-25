5457d5b5e4cd0858ba5060428355f21ab913973e
win-laeyee <winlae.yee@u.nus.edu>
2024-07-25 15:00:47 +0800

    Add 1 more constant and check file type for powerpoint slides

--- backend/src/utils/helper.py
+++ backend/src/utils/helper.py
@@ -1,5 +1,5 @@
 import os
-from backend.src.utils.constants import PDF_DOCUMENT, WORD_DOCUMENT, IMAGE, VIDEO
+from backend.src.utils.constants import PDF_DOCUMENT, WORD_DOCUMENT, IMAGE, VIDEO, PPT_SLIDE
 
 def check_file_type(file_path: str) -> str:
     _, ext = os.path.splitext(file_path)
@@ -8,8 +8,10 @@ def check_file_type(file_path: str) -> str:
     
     if ext == '.pdf':
         return PDF_DOCUMENT
-    elif ext == '.docx':
+    elif ext == '.docx' or ext == ".doc":
         return WORD_DOCUMENT
+    elif ext == '.pptx' or ext == '.ppt':
+        return PPT_SLIDE
     elif ext == '.mp4':
         return VIDEO
     elif ext == '.jpg' or ext == '.jpeg' or ext == '.png':

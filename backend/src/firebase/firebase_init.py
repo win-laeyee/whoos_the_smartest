378f9e2bcfcd8ce8830bb06add47d2cbea315923
win-laeyee <winlae.yee@u.nus.edu>
2024-07-28 21:38:50 +0800

    Initalise firebase

---
+++ backend/src/firebase/firebase_init.py
@@ -0,0 +1,17 @@
+import firebase_admin
+from firebase_admin import credentials
+import pyrebase
+import json
+
+
+def initialize_firebase():
+    if not firebase_admin._apps:
+        cred = credentials.Certificate("backend/secrets/firebase_service_account_key.json")
+        firebase_admin.initialize_app(cred)
+
+    with open('backend/secrets/firebase_sdk.json') as f:
+        firebase_config = json.load(f)
+    
+    firebase = pyrebase.initialize_app(firebase_config)
+        
+    return firebase

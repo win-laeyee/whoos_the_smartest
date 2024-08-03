8b18fb2984384ab8aa99c83ed1c8b9691a44f565
win-laeyee <winlae.yee@u.nus.edu>
2024-08-04 00:03:41 +0800

    Add quiz prompt to constants

--- backend/src/utils/constants.py
+++ backend/src/utils/constants.py
@@ -8,3 +8,22 @@ PPT_SLIDE = "PRESENTATION_SLIDES"
 USER_COLLECTION = 'users'
 NOTE_COLLECTION = 'notes'
 QUIZ_COLLECTION = 'quiz_qn_and_ans'
+
+
+QUIZ_FORMATTER = """Please return JSON describing the question and answer from this text using the following schema:
+
+    {"question_answer_list": list[MultipleChoice, MultiSelect, TrueFalse, FillInTheBlank, ShortAnswer, LongAnswer]}
+
+    MultipleChoice = {"question": str, "choices": list[str], "answer": int, "explanation": str}
+    MultiSelect = {"question": str, "choices": list[str], "answer": list[int], "explanation": str}
+    TrueFalse = {"question": str, "choices": ["True", "False"], "answer": str, "explanation": str}
+    FillInTheBlank = {"question": str, "answer": str, "explanation": str}
+    ShortAnswer = {"question": str, "answer": str, "explanation": str}
+    LongAnswer = {"question": str, "answer": str, "explanation": str}
+   
+    All other fields are required.
+
+    Important: Only return a single piece of valid JSON text.
+
+    Here is the text:
+    """

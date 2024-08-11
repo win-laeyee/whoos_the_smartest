bad2be7744c9c9a53231965212faad566e9b5636
win-laeyee <winlae.yee@u.nus.edu>
2024-08-11 16:30:32 +0800

    Import from .env instead of json files

---
+++ app/firebase.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import { useEffect, useRef } from "react";
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider, EmailAuthProvider } from "firebase/auth";
+import * as firebaseui from "firebaseui";
+import "firebaseui/dist/firebaseui.css";
+
+// Firebase configuration object
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+const FirebaseAuthUI: React.FC = () => {
+  const uiRef = useRef<firebaseui.auth.AuthUI | null>(null);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      if (!uiRef.current) {
+        if (firebaseui.auth.AuthUI.getInstance()) {
+          uiRef.current = firebaseui.auth.AuthUI.getInstance();
+        } else {
+          uiRef.current = new firebaseui.auth.AuthUI(auth);
+        }
+
+        if (uiRef.current) {
+          uiRef.current.start("#firebaseui-auth-container", {
+            signInSuccessUrl: "/upload",
+            signInFlow: "popup",
+            signInOptions: [
+              {
+                provider: GoogleAuthProvider.PROVIDER_ID,
+              },
+              {
+                provider: EmailAuthProvider.PROVIDER_ID,
+              },
+            ],
+            credentialHelper: firebaseui.auth.CredentialHelper.GOOGLE_YOLO,
+          });
+        }
+      }
+    }
+  }, []);
+
+  return <div id="firebaseui-auth-container"></div>;
+};
+
+export default FirebaseAuthUI;
+export { auth };
--- backend/src/utils/app_init.py
+++ backend/src/utils/app_init.py
@@ -1,5 +1,7 @@
 import json
 import logging
+from dotenv import load_dotenv
+import os
 
 import google.generativeai as genai
 from google.generativeai import GenerativeModel
@@ -10,15 +12,13 @@ from firebase_admin import credentials
 import pyrebase
 from pyrebase.pyrebase import Firebase
 
+load_dotenv()
 
 def configure_genai() -> None:
     """
     Configures the Google Generative AI API with the API key.
     """
-    with open('backend/secrets/google_gemini_credentials.json') as f:
-        secrets = json.load(f)
-
-    GOOGLE_API_KEY=secrets['GOOGLE_API_KEY']
+    GOOGLE_API_KEY=os.getenv("GOOGLE_API_KEY", "")
     genai.configure(api_key=GOOGLE_API_KEY)
     return GOOGLE_API_KEY
 
@@ -64,12 +64,32 @@ def initialize_firebase() -> Firebase:
     """
     Initializes Firebase with the provided service account and SDK configurations.
     """
+    firebase_service_cred = {
+        "type": os.getenv("FIREBASE_TYPE", ""),
+        "project_id": os.getenv("FIREBASE_PROJECT_ID", ""),
+        "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID", ""),
+        "private_key": os.getenv("FIREBASE_PRIVATE_KEY", "").replace("\\n", "\n"),
+        "client_email": os.getenv("FIREBASE_CLIENT_EMAIL", ""),
+        "client_id": os.getenv("FIREBASE_CLIENT_ID", ""),
+        "auth_uri": os.getenv("FIREBASE_AUTH_URI", ""),
+        "token_uri": os.getenv("FIREBASE_TOKEN_URI", ""),
+        "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_X509_CERT_URL", ""),
+        "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_X509_CERT_URL", ""),
+        "universe_domain": os.getenv("FIREBASE_UNIVERSE_DOMAIN", "")
+    }
     if not firebase_admin._apps:
-        cred = credentials.Certificate("backend/secrets/firebase_service_account_key.json")
+        cred = credentials.Certificate(firebase_service_cred)
         firebase_admin.initialize_app(cred)
 
-    with open('backend/secrets/firebase_sdk.json') as f:
-        firebase_config = json.load(f)
+    firebase_config = {
+        "apiKey": os.getenv("FIREBASE_API_KEY", ""),
+        "authDomain": os.getenv("AUTH_DOMAIN", ""),
+        "projectId": os.getenv("PROJECT_ID", ""),
+        "storageBucket": os.getenv("STORAGE_BUCKET", ""),
+        "messagingSenderId": os.getenv("MESSAGING_SENDER_ID", ""),
+        "appId": os.getenv("APP_ID", ""),
+        "databaseURL": os.getenv("DATABASE_URL", "")
+        }
 
     firebase = pyrebase.initialize_app(firebase_config)
 

ab208898f4c8e010d1ef457669a78feadfb21d50
win-laeyee <winlae.yee@u.nus.edu>
2024-08-11 17:08:00 +0800

    Change to use call back to remove client side error

--- app/firebase.tsx
+++ app/firebase.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useRef } from "react";
+import { useEffect, useCallback } from "react";
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, EmailAuthProvider } from "firebase/auth";
 import * as firebaseui from "firebaseui";
@@ -20,38 +20,30 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
 const FirebaseAuthUI: React.FC = () => {
-  const uiRef = useRef<firebaseui.auth.AuthUI | null>(null);
+  const loadFirebaseui = useCallback(async () => {
+    const firebaseui = await import("firebaseui");
+    const firebaseUi =
+      firebaseui.auth.AuthUI.getInstance() || new firebaseui.auth.AuthUI(auth);
+    firebaseUi.start("#firebaseui-auth-container", {
+      signInSuccessUrl: "/upload",
+      signInFlow: "popup",
+      signInOptions: [
+        {
+          provider: GoogleAuthProvider.PROVIDER_ID,
+        },
+        {
+          provider: EmailAuthProvider.PROVIDER_ID,
+        },
+      ],
+      credentialHelper: firebaseui.auth.CredentialHelper.GOOGLE_YOLO,
+    });
+  }, []);
 
   useEffect(() => {
-    if (typeof window !== "undefined") {
-      if (!uiRef.current) {
-        if (firebaseui.auth.AuthUI.getInstance()) {
-          uiRef.current = firebaseui.auth.AuthUI.getInstance();
-        } else {
-          uiRef.current = new firebaseui.auth.AuthUI(auth);
-        }
-
-        if (uiRef.current) {
-          uiRef.current.start("#firebaseui-auth-container", {
-            signInSuccessUrl: "/upload",
-            signInFlow: "popup",
-            signInOptions: [
-              {
-                provider: GoogleAuthProvider.PROVIDER_ID,
-              },
-              {
-                provider: EmailAuthProvider.PROVIDER_ID,
-              },
-            ],
-            credentialHelper: firebaseui.auth.CredentialHelper.GOOGLE_YOLO,
-          });
-        }
-      }
-    }
+    loadFirebaseui();
   }, []);
 
   return <div id="firebaseui-auth-container"></div>;
 };
-
 export default FirebaseAuthUI;
 export { auth };

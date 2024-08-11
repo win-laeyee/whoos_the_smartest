5bccbd06d0ec4f09a72e6911c4790479acdf5c64
sharmaine1028 <sharmaine1028@gmail.com>
2024-08-11 13:53:26 +0800

    Edit favicon

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -17,23 +17,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="icon" href="/favicon.png" />
         <meta name="theme-color" content="#000000" />
-        {/* Add additional icon sizes if needed */}
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        {/* Add other head elements if needed */}
       </head>
       <body className={inter.className}>{children}</body>
     </html>

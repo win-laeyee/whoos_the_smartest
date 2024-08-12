0bfb0212d42009603989b4318281ec91a11d7635
win-laeyee <winlae.yee@u.nus.edu>
2024-08-12 17:50:02 +0800

    Update firebase.tsx with empty strings and remove firebase_example.tsx

--- frontend/src/app/firebase.tsx
+++ frontend/src/app/firebase.tsx
@@ -8,12 +8,12 @@ import "firebaseui/dist/firebaseui.css";
 
 // Firebase configuration object
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_APP_ID,
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 const app = initializeApp(firebaseConfig);

14046a7893fca83d34fc86f820561e270152ebbb
win-laeyee <winlae.yee@u.nus.edu>
2024-08-17 16:24:31 +0800

    Change error message to unsupported file type

--- backend/src/utils/notes/file_management/file_check.py
+++ backend/src/utils/notes/file_management/file_check.py
@@ -1,6 +1,6 @@
 import os
 
-from backend.src.utils.constants import IMAGE, PDF_DOCUMENT, PPT_SLIDE, VIDEO, WORD_DOCUMENT
+from backend.src.utils.constants import IMAGE, PDF_DOCUMENT, VIDEO, WORD_DOCUMENT #,PPT_SLIDE
 
 
 def check_file_type(file_name: str) -> str:
@@ -24,11 +24,11 @@ def check_file_type(file_name: str) -> str:
         return PDF_DOCUMENT, ext
     elif ext == '.docx':
         return WORD_DOCUMENT, ext
-    elif ext == '.pptx':
-        return PPT_SLIDE, ext
+    # elif ext == '.pptx':
+    #     return PPT_SLIDE, ext
     elif ext == '.mp4' or ext == '.mov':
         return VIDEO, ext
     elif ext == '.jpg' or ext == '.jpeg' or ext == '.png':
         return IMAGE, ext
     else:
-        raise ValueError("Unknown File Type")
+        raise ValueError("Unsupported File Type")

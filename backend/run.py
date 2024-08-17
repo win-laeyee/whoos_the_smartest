6fa67c3b8ac0ecfd6433e06751e41c3fba485cce
win-laeyee <winlae.yee@u.nus.edu>
2024-08-17 16:17:52 +0800

    Run app directly

--- backend/run.py
+++ backend/run.py
@@ -1,4 +1,9 @@
+from backend.src.api.main import app
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("backend.src.api.main:app", host="0.0.0.0", port=8000, workers=1, reload=True)
+    uvicorn.run(app, host="0.0.0.0", port=8000, workers=1, reload=True)
+
+# if __name__ == "__main__":
+#     uvicorn.run("backend.src.api.main:app", host="0.0.0.0", port=8000, workers=1, reload=True)
+
